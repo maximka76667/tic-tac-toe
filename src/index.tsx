971dabc9051888207b5a2949bc1c9b3a89712209
max76667 <max766667@gmail.com>
2021-08-31 16:18:08 +0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/tic-tac-toe">
       <App />
     </HashRouter>
   </React.StrictMode>,
