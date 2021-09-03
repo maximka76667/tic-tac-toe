afb1d97cb5900049b03bf967d0ed0837ea691737
max76667 <max766667@gmail.com>
2021-09-03 16:31:01 +0600

    menu primary styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <div className="menu">
-      <Link to="/single">1 player</Link>
-      <Link to="/multi">2 Players</Link>
+      <h1 className="menu__heading">Tic Tac Toe</h1>
+      <div className="menu__game-mode"><Link className="menu__game-mode-link" to="/single">1 Player</Link></div>
+      <div className="menu__game-mode"><Link className="menu__game-mode-link" to="/multi">2 Players</Link></div>
     </div>
   )
 }
